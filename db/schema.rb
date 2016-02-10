579cba26afb849915eefa024900ca2adb57f5fb8
Kevin Hobbs <kevin21921@gmail.com>
2016-02-10 16:50:36 -0500

    Updated schema to include has_many associations.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210132932) do
+ActiveRecord::Schema.define(version: 20160210212823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.date     "start_date"
+    t.date     "end_date"
+    t.time     "start_time"
+    t.time     "end_time"
+    t.string   "event_name"
+    t.text     "event_desc"
+    t.decimal  "cost"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "userevents", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "userevents", ["event_id"], name: "index_userevents_on_event_id", using: :btree
+  add_index "userevents", ["user_id"], name: "index_userevents_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -38,4 +60,6 @@ ActiveRecord::Schema.define(version: 20160210132932) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "userevents", "events"
+  add_foreign_key "userevents", "users"
 end
