e9d4a939e078ce55282b9f30ea38bf0b553d8ae2
Kevin Hobbs <kevin21921@gmail.com>
2016-02-16 11:49:20 -0500

    Save slim.html.erb for parallax fixes.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,5 +34,30 @@ $(document).ready(function(){
 
   $('.datepicker').datepicker();
 
+
+  // Parallax init JS
+  
+
+  $('section[data-type="background"]').each(function(){
+   // declare the variable to affect the defined data-type
+   var $scroll = $(this);
+                   
+    $(window).scroll(function() {
+      // HTML5 proves useful for helping with creating JS functions!
+      // also, negative value because we're scrolling upwards                            
+      var yPos = -($window.scrollTop() / $scroll.data('speed'));
+       
+      // background position
+      var coords = '50% '+ yPos + 'px';
+
+      // move the background
+      $scroll.css({ backgroundPosition: coords });   
+    }); // end window scroll
+  });  // end section function
+
 });
+
+
+  
+
 
