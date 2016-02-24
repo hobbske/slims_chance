f4dc0fd40a60e9f60a8a3004e610e521809f155b
Kevin Hobbs <kevin21921@gmail.com>
2016-02-24 15:05:11 -0500

    Added contacts table and model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211020431) do
+ActiveRecord::Schema.define(version: 20160224200055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "comments"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "events", force: :cascade do |t|
     t.date     "start_date"
